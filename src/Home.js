87598d065754aaa6989415bc6173509c4d465f34
trawallybakary7@gmail.com <trawallybakary7@gmail.com>
2025-08-08 22:17:19 +0000

    first commit

---
+++ src/Home.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import Feed from './Feed'
+
+const Home = ({ posts }) => {
+  return (
+    <div className='Home'>
+      { posts.length ? (
+        <Feed posts={posts}/>
+      ) : (
+        <p style={{ marginTop: "2rem"}}>
+          No posts to display
+        </p>
+      )}
+    </div>
+  )
+}
+
+export default Home
